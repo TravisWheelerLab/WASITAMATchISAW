646a389fcd4e75d6fc797c738b069e8e75e7b6d4
George Glidden <georgeglidden@protonmail.com>
2023-07-10 07:36:49 -0700

    the BLAST module implements blastp, makeblastdb, convert2blastmask, and cleanup functions. each function returns the result of its command, as printed to stdout, via the read(::Cmd) interface wrapped by BLAST.execute(::String).

---
+++ source/blast.jl
@@ -0,0 +1,81 @@
+module BLAST
+
+    execute(command::String) = read(Cmd(String.(split(command, ' '))), String)
+
+    function blastp(
+        pathtoqueryseq::String,
+        pathtotargetdb::String;
+        pathtomaskdb::String="",
+    )
+        command = "blastp -query $(pathtoqueryseq) -db $(pathtotargetdb) -word_size 4 -outfmt 7"
+        if length(pathtomaskdb) > 0
+            command *= "-commanddb_hard_mask 100"
+        end
+        execute(command)
+    end
+
+    # TODO
+    function blastn()
+        nothing
+    end
+
+    function makeblastdb(
+        pathtotargetseq::String,
+        pathtoresult::String;
+        pathtomaskdb::String="",
+    )
+        command = "makeblastdb -in $(pathtotargetseq) -dbtype prot -out $(pathtoresult) -title $(pathtoresult)"
+        if length(pathtomaskdb) > 0
+            command *= " -mask_data $(pathtomaskdb)"
+        end
+        execute(command)
+    end
+
+    function convert2blastmask(
+        pathtotargetmask::AbstractString,
+        pathtoresult::AbstractString,
+    )
+        pathtomaskdb = pathtoresult * ".msk"
+        command = "convert2blastmask -in $(pathtotargetmask) -out $(pathtomaskdb) -parse_seqids -masking_algorithm tantan -masking_options \"-p\" "
+        execute(command)
+    end
+
+    function cleanup_makeblastdb(
+        pathtoresult::AbstractString,
+    )
+        commands = [
+            "rm $(pathtoresult * ".pto")",
+            "rm $(pathtoresult * ".ptf")",
+            "rm $(pathtoresult * ".psq")",
+            "rm $(pathtoresult * ".pog")",
+            "rm $(pathtoresult * ".pot")",
+            "rm $(pathtoresult * ".pos")",
+            "rm $(pathtoresult * ".pin")",
+            "rm $(pathtoresult * ".phr")",
+            "rm $(pathtoresult * ".pdb")"]
+        for command=commands
+            try
+                execute(command)
+            catch
+                ; # throws an exception when one of the files it tries to rm doesn't exist
+            end
+        end
+    end
+    
+    function cleanup_convert2blastmask(
+        pathtoresult::AbstractString,
+    )
+        commands = [
+            "rm $(pathtoresult * ".msk")",
+            "rm $(pathtoresult * ".paa")",
+            "rm $(pathtoresult * ".pab")",
+            "rm $(pathtoresult * ".pac")"]
+        for command=commands
+            try
+                execute(command)
+            catch
+                ;
+            end
+        end
+    end
+end
