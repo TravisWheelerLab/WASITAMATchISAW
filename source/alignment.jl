ae7917c18fb88548aaa28783b835725a67ec0ff1
George Glidden <47768122+georgeglidden@users.noreply.github.com>
2023-07-20 11:47:10 -0700

    alignment.jl implements multithreaded alignments for Pairwise, OneToMany, and ManyToMany comparisons.

--- source/alignment.jl
+++ source/alignment.jl
@@ -1,9 +1,9 @@
-using Base.Iterators: Stateful, popfirst!
+using Base.Iterators: Stateful, popfirst!, repeated
 using Base.Threads: @threads
-using BioAlignments: LocalAlignment, AffineGapScoreModel, BLOSUM62, pairalign
-using ProgressMeter: Progress, next!
+using BioAlignments: PairwiseAlignmentResult, LocalAlignment, AffineGapScoreModel, BLOSUM62, pairalign
+using ProgressMeter: Progress, next!, @showprogress
 
-function paired_alignment(
+function _align(
     pairs::Base.Iterators.Zip,
     n::Int,
     model,
@@ -26,6 +26,71 @@ function paired_alignment(
         (query, reference) = popfirst!(pairgenerator)
         res = pairalign(model, query, reference, schema)
         results[i] = formatter(res)
+        if verbose
+            next!(p)
+        end
+    end
+    results
+end
+
+abstract type AlignmentProductSpace end
+struct Pairwise <: AlignmentProductSpace end
+struct OneToMany <: AlignmentProductSpace end
+struct ManyToMany <: AlignmentProductSpace end
+
+function align(
+    ::Pairwise,
+    queries,
+    references;
+    model=LocalAlignment(),
+    schema=AffineGapScoreModel(BLOSUM62, -11, -1),
+    formatter=x::PairwiseAlignmentResult->x,
+    verbose=false,
+)
+    n = length(queries)
+    if length(references) != n
+        throw(ArgumentError("Pairwise alignment requires there be an equal number of query and reference sequences."))
+    end
+    _align(zip(queries, references), n, model, schema, formatter, verbose)
+end
+
+function align(
+    ::OneToMany,
+    query,
+    references;
+    model=LocalAlignment(),
+    schema=AffineGapScoreModel(BLOSUM62, -11, -1),
+    formatter=x::PairwiseAlignmentResult->x,
+    verbose=false,
+)
+    n = length(references)
+    _align(zip(repeated(query), references), n, model, schema, formatter, verbose)
+end
+
+function align(
+    ::ManyToMany,
+    queries,
+    references;
+    model=LocalAlignment(),
+    schema=AffineGapScoreModel(BLOSUM62, -11, -1),
+    formatter=x::PairwiseAlignmentResult->x,
+    verbose=false,
+)
+    n = length(queries)
+    m = length(references)
+    result_type = Union{Base.return_types(formatter)...}
+    # create a place for each result
+    results = Vector{result_type}(undef, n*m)
+    if verbose
+        p = Progress(n, 0.1, "Aligning...")
+    end
+    for i=1:n
+        results[1+(i-1)*m:i*m] = align(
+            OneToMany(),
+            queries[i],
+            references,
+            model=model, schema=schema, formatter=formatter,
+            verbose=false)
         if verbose
             next!(p)
         end
