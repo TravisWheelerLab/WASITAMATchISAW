252e0efbbdd245dd44104f6f9c902e575d9d24d2
George Glidden <47768122+georgeglidden@users.noreply.github.com>
2023-07-26 13:22:57 -0700

    reworked script for generating palindrome distribution in tryptic peptides

--- fig7.jl
+++ fig7.jl
@@ -1,34 +1,29 @@
-using FASTX: sequence, FASTARecord, FASTAWriter
-using ProgressMeter: @showprogress, Progress, next!
-using Base.Threads: @threads, nthreads
-using Random: shuffle!
-include("source/io.jl")
-include("source/trypsin.jl")
-include("source/palindrome.jl")
-### partition Swiss-Prot into `nthreads` batches
-sprot = [record for record=readfasta("data/sprot.fa") if 50 < length(sequence(record)) < 2000]
-nsprot = length(sprot)
-batch_size = ceil(Int, nsprot / nthreads())
-sprot_batches = [sprot[1+batch_size*(i-1):min(nsprot, batch_size*i)] for i=1:nthreads()]
-length.(sprot_batches)
+# figure 7
+### distribution of LPS and LCS in tryptic peptides of Swiss-Prot
+using Base.Threads: @threads
+using FASTX: sequence
+using ProgressMeter: Progress, next!
+using DelimitedFiles: writedlm
 ### LPS and LCS
+include("source/io.jl");
+include("source/trypsin.jl");
+include("source/palindrome.jl");
+sprotall = [record for record=readfasta("data/sprot.fa") if 50 < length(sequence(record)) <= 2000]
+nsprotall = length(sprotall)
+palindromedistribution = zeros(Int, (2000, 2000))
 minlength = 5
 maxlength = 100
-p = Progress(length(sprot), 1, "Digesting...")
-tempfiles = ["outputs/.temp_trypticpalindromes_thread$(t).txt" for t=1:nthreads()]
-@threads for t=1:nthreads()
-    open(tempfiles[t], "w") do writer
-        for i=1:length(sprot_batches[t])
-            seq = sequence(sprot_batches[t][i])
-            trypticintervals = trypticpeptides(seq, minlength, maxlength)
-            trypticsequences = (seq[start:stop] for (start,stop)=trypticintervals)
-            lps = longestpalindromicsubstring.(trypticsequences)
-            lps = (x->"\t$(x[1]),$(x[2])").(lps)
-            write(writer, "$((t-1)*batch_size+i)")
-            write.(writer, lps)
-            write(writer, '\n')
-            flush(writer)
-            next!(p)
-        end
+p = Progress(length(sprotall), 1, "Digesting...")
+@threads for i=1:nsprotall
+    seq = sequence(sprotall[i])
+    trypticintervals = trypticpeptides(seq, minlength, maxlength)
+    trypticsequences = (seq[start:stop] for (start,stop)=trypticintervals)
+    lps = longestpalindromicsubstring.(trypticsequences)
+    for (start, stop)=lps
+        palindromelength = stop - start
+        sequencelength = length(seq)
+        palindromedistribution[palindromelength, sequencelength] += 1
     end
+    next!(p)
 end
+writedlm("outputs/sprot_all-tryptic-palindrome-distribution.dlm", palindromedistribution)
