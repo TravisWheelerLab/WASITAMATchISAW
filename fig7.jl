dddba24eab47249d78398b3187bee40fbb0c524e
George Glidden <47768122+georgeglidden@users.noreply.github.com>
2023-07-27 01:02:52 -0700

    implemented string views

--- fig7.jl
+++ fig7.jl
@@ -1,14 +1,38 @@
 # figure 7
-### distribution of LPS and LCS in tryptic peptides of Swiss-Prot
 using Base.Threads: @threads
 using FASTX: sequence
 using ProgressMeter: Progress, next!
 using DelimitedFiles: writedlm
-### LPS and LCS
+
 include("source/io.jl");
 include("source/trypsin.jl");
 include("source/palindrome.jl");
+# method 1 - broadcast view
+println("method 1 - broadcast view")
 sprotall = [record for record=readfasta("data/sprot.fa") if 50 < length(sequence(record)) <= 2000]
+sprotall = sprotall[1:12000]
+nsprotall = length(sprotall)
+palindromedistribution = zeros(Int, (2000, 2000))
+minlength = 5
+maxlength = 100
+p = Progress(length(sprotall), 1, "Digesting...")
+@threads for i=1:nsprotall
+    seq = sequence(sprotall[i])
+    trypticintervals = trypticpeptides(seq, minlength, maxlength)
+    trypticsequences = view.(seq, (start:stop for (start, stop)=trypticintervals))
+    lps = longestpalindromicsubstring.(trypticsequences)
+    for (start, stop)=lps
+        palindromelength = stop - start
+        sequencelength = length(seq)
+        palindromedistribution[palindromelength, sequencelength] += 1
+    end
+    next!(p)
+end
+writedlm("outputs/sprot_all-tryptic-palindrome-distribution.dlm", palindromedistribution)
+# method 2 - lazily generate substrings
+println("method 2 - lazily generate substrings")
+sprotall = [record for record=readfasta("data/sprot.fa") if 50 < length(sequence(record)) <= 2000]
+sprotall = sprotall[1:12000]
 nsprotall = length(sprotall)
 palindromedistribution = zeros(Int, (2000, 2000))
 minlength = 5
