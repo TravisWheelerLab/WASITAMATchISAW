1c0b0d8a50b3340007ea317006efe9a502c32db7
George Glidden <47768122+georgeglidden@users.noreply.github.com>
2023-10-10 16:47:06 -0700

    wrote docs, parsing scripts for GffRead step. scaffolded around the (todo) sampling code

--- source/chromosome.jl
+++ source/chromosome.jl
@@ -19,20 +19,12 @@ end
 
 \(annotation::GTFAnnotation, rowmask::BitVector) = GTFAnnotation(annotation.table[rowmask, :])
 
-chromosome(n::Int, annotation::GTFAnnotation) = annotation \ contains.(annotation.table.chromosome, "chr$(n)")
+chromosome(n::Int, annotation::GTFAnnotation) = annotation \ (annotation.table.chromosome .== "chr$(n)")
 
-exon(annotation::GTFAnnotation) = annotation \ annotation.table.type .== "exon"
+exon(annotation::GTFAnnotation) = annotation \ (annotation.table.type .== "exon")        
 
-function intron(annotation::GTFAnnotation)
-    return -1
-    # every gene has a sequence of indices bracketed by the gene's start and stop locations and filled in the middle by the start and stop of each exon. introns are from the stop one of exon to the start of another, ie the first intron is from gene-start to intron-1-stop. we'll have to extract each of these indices, put them in order, then iterate pairs via 2-steps.
-    # note - subsample! some introns are a million bp.
-end
+intron(annotation::GTFAnnotation) = annotation \ (annotation.table.type .== "3UTR" .|| annotation.table.type == "5UTR")
 
-function intergenic(annotation::GTFAnnotation)
-    # from gene-n-stop to gene-{n+1}-start
-    # going to have to subsample bc intergenic regions are even bigger than introns.
-end
+intragenic(annotation::GTFAnnotation) = annotation \ (annotation.table.type .== "transcript")
 
 const GTFCOLUMNS = ["chromosome","version","type","start","stop","dot","parity","zero","metadata"]
-
